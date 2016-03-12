d18e6648698484d2deeef94ba4f53bafb58f2fd4
Eric Sérandour <internet1@serandour.org>
2016-03-12 23:35:25 +0100

    Update Demo.ino

--- examples/Demo/Demo.ino
+++ examples/Demo/Demo.ino
@@ -27,18 +27,6 @@
 
 ////////////////////////////////////////////////////////////////////////
 
-Branchements :
-
-Dos du Minitel           Carte Arduino
-
-DIN 1 : RX   <-------->  Digital Pin 8 par exemple (TX)
-DIN 4 :      <---//--->
-DIN 2 : GND  <-------->  GND
-DIN 5 :      <---//--->
-DIN 3 : TX   <-------->  Digital Pin 7 par exemple (RX)
-
-////////////////////////////////////////////////////////////////////////
-
 
 Paramètres disponibles pour attributs(byte attribut)
 --------------------------------------------------------
