cfa8ce0fe1444cb5c09bff6d02f4b7c15c17f130
3615 <internet-1@serandour.org>
2021-06-28 18:08:49 +0200

    Update Minitel1B_Soft.h

--- Minitel1B_Soft.h
+++ Minitel1B_Soft.h
@@ -1,6 +1,6 @@
-////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 /*
-   Minitel1B_Soft - Fichier d'en-tête - Version du 28 juin 2021 à 10h38
+   Minitel1B_Soft - Fichier d'en-tête - Version du 28 juin 2021 à 17h22
    Copyright 2016-2021 - Eric Sérandour
    http://3615.entropie.org
    
@@ -203,6 +203,9 @@
 #define CODE_RECEPTION_MODEM    0x5A
 #define CODE_RECEPTION_PRISE    0x5B
 
+#define AIGUILLAGE_OFF             0x60
+#define AIGUILLAGE_ON              0x61
+
 #define PROG                    0x6B
 #define STATUS_VITESSE          0x74
 #define START                   0x69
@@ -314,6 +317,10 @@ public:
   int capitalMode();  // Mode majuscules du clavier
   int extendedKeyboard();  // Clavier étendu
   int standardKeyboard();  // Clavier standard
+  void echo(boolean commande);  // Active ou désactive l'écho à l'écran de ce qui est tapé au clavier
+  
+  // Protocole
+  void aiguillage(boolean commande, byte emetteur, byte recepteur);
 
 private: 
   byte currentSize = GRANDEUR_NORMALE;
