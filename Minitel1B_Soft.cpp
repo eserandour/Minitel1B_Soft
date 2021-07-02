77b45f452c6df7843f059e67653001a5a0986ee3
3615 <internet-1@serandour.org>
2021-07-02 15:32:02 +0200

    Update Minitel1B_Soft.cpp

--- Minitel1B_Soft.cpp
+++ Minitel1B_Soft.cpp
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////
 /*
-   Minitel1B_Soft - Fichier source - Version du 01 juillet 2021 à 22h03
+   Minitel1B_Soft - Fichier source - Version du 02 juillet 2021 à 15h27
    Copyright 2016-2021 - Eric Sérandour
    http://3615.entropie.org
 
